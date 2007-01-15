163174c2ab4a10775ee9d037ec7594cc82eb1d18
rjan <rjan@c960cec6-6b13-0410-98af-d8f80e9b129c>
2007-01-15 15:42:49 +0000

    git-svn-id: https://r-osgi.svn.sourceforge.net/svnroot/r-osgi/trunk@720 c960cec6-6b13-0410-98af-d8f80e9b129c

--- http/channel/src/main/java/ch/ethz/iks/r_osgi/http/HttpChannelFactory.java
+++ http/channel/src/main/java/ch/ethz/iks/r_osgi/http/HttpChannelFactory.java
@@ -196,14 +196,17 @@ final class HttpChannelFactory implements NetworkChannelFactory {
 			connection.setDoOutput(true);
 			message.send(new ObjectOutputStream(connection.getOutputStream()));
 
+			System.out.println("A");
 			final ObjectInputStream in = new ObjectInputStream(connection
 					.getInputStream());
 
+			System.out.println("B");
 			if (message.getFuncID() == RemoteOSGiMessage.LEASE) {
 				System.out.println("USING " + connection + " FOR CALLBACK");
 				new CallbackThread(in).start();
 				return;
 			}
+			System.out.println("C");
 
 			final RemoteOSGiMessage msg = RemoteOSGiMessage.parse(in);
 			System.out.println("received " + msg);
