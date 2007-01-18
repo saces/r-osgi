7129fc0f2e32e83a535f2852cdfa18d3760fea74
rjan <rjan@c960cec6-6b13-0410-98af-d8f80e9b129c>
2007-01-18 15:55:39 +0000

    trying to go back to working version.
    git-svn-id: https://r-osgi.svn.sourceforge.net/svnroot/r-osgi/trunk@738 c960cec6-6b13-0410-98af-d8f80e9b129c

--- http/channel/src/main/java/ch/ethz/iks/r_osgi/http/HttpChannelFactory.java
+++ http/channel/src/main/java/ch/ethz/iks/r_osgi/http/HttpChannelFactory.java
@@ -196,12 +196,17 @@ final class HttpChannelFactory implements NetworkChannelFactory {
 			connection.setDoOutput(true);
 			message.send(new ObjectOutputStream(connection.getOutputStream()));
 
+			System.out.println("A");
 			final ObjectInputStream in = new ObjectInputStream(connection
 					.getInputStream());
+
+			System.out.println("B");
 			if (message.getFuncID() == RemoteOSGiMessage.LEASE) {
+				System.out.println("USING " + connection + " FOR CALLBACK");
 				new CallbackThread(in).start();
 				return;
 			}
+			System.out.println("C");
 
 			final RemoteOSGiMessage msg = RemoteOSGiMessage.parse(in);
 			System.out.println("received " + msg);
@@ -216,14 +221,16 @@ final class HttpChannelFactory implements NetworkChannelFactory {
 			}
 
 			public void run() {
-				try {
-					while (!Thread.interrupted()) {
+				while (!Thread.interrupted()) {
+					try {
+
+						System.out.println("READY TO RECEIVE...");
 						RemoteOSGiMessage msg = RemoteOSGiMessage.parse(input);
 						System.out.println("asynchronously received " + msg);
 						endpoint.receivedMessage(msg);
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
 				}
 			}
 		}
