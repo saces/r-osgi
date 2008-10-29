84efd398f304836a1b78a285513c8322d66edc0f
rjan <rjan@c960cec6-6b13-0410-98af-d8f80e9b129c>
2008-10-29 01:01:38 +0000

    added a dependency bundle for testing.
    git-svn-id: https://r-osgi.svn.sourceforge.net/svnroot/r-osgi/trunk@1195 c960cec6-6b13-0410-98af-d8f80e9b129c

---
+++ sample/dependency/src/ch/ethz/iks/r_osgi/sample/dependency/Message.java
@@ -0,0 +1,15 @@
+package ch.ethz.iks.r_osgi.sample.dependency;
+
+public class Message {
+
+	private String msg;
+	
+	public Message(String msg) {
+		this.msg = msg;
+	}
+	
+	public String toString() {
+		return msg;
+	}
+	
+}
